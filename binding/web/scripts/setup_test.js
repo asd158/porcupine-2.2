c15b6010aaccd706b52defd8452dbb80cb3a708b
asd158 <gaowycn@qq.com>
2023-05-19 11:00:17 +0800

    +

---
+++ binding/web/scripts/setup_test.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const { join } = require('path');
+
+console.log('Copying the porcupine models...');
+
+const testDirectory = join(__dirname, '..', 'test');
+const fixturesDirectory = join(__dirname, '..', 'cypress', 'fixtures');
+
+const paramsSourceDirectory = join(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  'lib',
+  'common',
+);
+
+const testDataSource = join(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  'resources',
+  '.test',
+  'test_data.json'
+);
+
+
+console.log("Copying the PPN model...");
+
+const sourceDirectory = join(
+  __dirname,
+  "..",
+  "..",
+  "..",
+  "resources",
+);
+
+try {
+  fs.mkdirSync(testDirectory, { recursive: true });
+
+  fs.readdirSync(paramsSourceDirectory).forEach(file => {
+    fs.copyFileSync(join(paramsSourceDirectory, file), join(testDirectory, file));
+  });
+
+  fs.copyFileSync(testDataSource, join(testDirectory, 'test_data.json'));
+
+  fs.mkdirSync(join(testDirectory, 'keywords'), { recursive: true });
+  fs.readdirSync(sourceDirectory).forEach(folder => {
+    if (folder.includes("keyword_files")) {
+      fs.readdirSync(join(sourceDirectory, folder, 'wasm')).forEach(file => {
+        fs.copyFileSync(join(sourceDirectory, folder, 'wasm', file), join(testDirectory, 'keywords', file.replace("ż", "z")));
+      })
+    }
+  });
+
+  fs.mkdirSync(join(fixturesDirectory, 'audio_samples'), { recursive: true });
+  fs.readdirSync(join(sourceDirectory, 'audio_samples')).forEach(file => {
+    fs.copyFileSync(join(sourceDirectory, 'audio_samples', file), join(fixturesDirectory, 'audio_samples', file));
+  });
+} catch (error) {
+  console.error(error);
+}
+
+console.log('... Done!');
